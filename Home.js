ee2245bd448eba9fa6d9e743b27ae795cf2b08c7
doghmoh <doghmoh@gmail.com>
2022-10-17 20:39:31 +0100

    first commit

---
+++ Home.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    var myflag = true;
+    $("#poper").click(function() {
+        if (myflag == true) {
+            $(".navbar-nav").css("left", "0");
+            myflag = false;
+        } else if (myflag == false) {
+            $(".navbar-nav").css("left", "-100%");
+            myflag = true;
+        }
+    })
+    $("#footer").load("002/Footer.html")
+
+});
